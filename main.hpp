e4fdb3d7b583d4472a1f6d33cf4f1f191b37d96e
Exit::Lag <vihaan.xml@gmail.com>
2026-09-21 19:05:58 -0400

    Update main.hpp

--- main.hpp
+++ main.hpp
@@ -1,1 +1,38 @@
+#pragma once
 
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+namespace taskscheduler
+{
+    class TaskScheduler
+    {
+    public:
+        TaskScheduler();
+        ~TaskScheduler();
+
+        TaskScheduler(const TaskScheduler&) = delete;
+        TaskScheduler& operator=(const TaskScheduler&) = delete;
+
+        void schedule(std::function<void()> task);
+
+        void start();
+        void stop();
+
+        bool isRunning() const;
+
+    private:
+        void worker();
+
+        std::queue<std::function<void()>> m_queue;
+        std::mutex m_mutex;
+        std::condition_variable m_condition;
+        std::thread m_thread;
+
+        bool m_running = false;
+        bool m_stopping = false;
+    };
+}
